a5b042af653500d7cce2f144715fbe0e0908c45b
jmarinllao <josepmarinllao@gmail.com>
2019-02-11 15:02:58 +0100

    Matrix iterator and check_scores function in notebook

--- src/diffupy/matrix.py
+++ src/diffupy/matrix.py
@@ -13,22 +13,16 @@ class Matrix:
     """Matrix class."""
 
     # TODO: why kw?
-    def __init__(self, mat, name='', rows_labels=None, cols_labels=None, graph=None, dupl=False, **kw):
+    def __init__(self, mat, rows_labels=None, cols_labels=None, name='', graph=None, dupl=False, **kw):
         """Initialize matrix."""
 
-        if rows_labels is None:
-            self._rows_labels = []
-        else:
-            self._rows_labels = rows_labels
+        self._rows_labels = rows_labels
 
-        if cols_labels is None:
-            self.cols_labels = []
-        else:
-            self._cols_labels = cols_labels
+        self._cols_labels = cols_labels
 
         self._name = name
-        self._mat = np.array(mat)
         self._dupl = dupl
+        self._mat = np.array(mat)
 
         if graph:
             self._rows_labels = get_label_list_graph(graph, 'name')
@@ -39,9 +33,30 @@ class Matrix:
         return f"matrix {self.name} \n {self.mat} \n row labels: {self.rows_labels} " \
                f"\n column labels: \n {self.cols_labels} \n : "
 
+
+    """Iterator"""
+    def __iter__(self):
+        self.i = -1
+        self.j = 0
+        return self
+
+    def __next__(self):
+        if self.j >= len(self.rows_labels)-1 and self.i >= len(self.cols_labels)-1:
+            raise StopIteration
+
+        if self.i >= len(self.cols_labels)-1:
+            self.i = 0
+            self.j += 1
+        else:
+            self.i += 1
+
+        if len(self.rows_labels) == 1: return self.mat[self.i], self.cols_labels[self.i], self.rows_labels[self.j]
+        return self.mat[self.j][self.i], self.cols_labels[self.i], self.rows_labels[self.j]
+
+
     """Getters and Setters"""
 
-    # TODO: Matrix?
+    # Raw matrix (numpy array)
     @property
     def mat(self):
         return self._mat
@@ -83,6 +98,7 @@ class Matrix:
         else:
             self._cols_labels = list(cols_labels)
 
+    # From labels
     def set_row_from_label(self, label, x):
         i_row = self.label_id_mapping[label]
         if isinstance(i_row, dict):
@@ -129,9 +145,10 @@ class Matrix:
                      for x in a[1:]]
                     for a in m[1:]
                 ]),
-            name=str(os.path.basename(path).split('.csv')),
             rows_labels=m[1:, 0],
-            cols_labels=m[0, 1:])
+            cols_labels=m[0, 1:],
+            name=str(os.path.basename(path).split('.csv'))
+        )
 
 
 # TODO: Poner que es matriz simetrica
