3716dafdf52a54dbb02a476750a59a33fd1df9e5
jmarinllao <josepmarinllao@gmail.com>
2019-05-02 17:00:38 +0200

    regularized_kernel_from_bel_graph in cli

--- src/diffuPy/cli.py
+++ src/diffuPy/cli.py
@@ -3,8 +3,16 @@
 """Command line interface."""
 
 import logging
+import os
+import time
 
 import click
+import pybel
+from pathme.constants import PATHME_DIR
+
+import networkx as nx
+
+from diffuPy.kernels import regularised_laplacian_kernel
 
 log = logging.getLogger(__name__)
 
@@ -18,10 +26,33 @@ def main():
 """Diffupy"""
 
 
-@main.group()
-def diffupy():
-    """Run experiment."""
+@main.command()
+@click.option('-g', '--graph_path', help='Input graph path.',
+              default=os.path.join(PATHME_DIR, 'universe', 'pathme_universe_bel_graph_no_flatten.bel.pickle'),
+              show_default=True)
+@click.option('-o', '--output', help='Output kernel pickle.',
+              default=os.path.join(PATHME_DIR, 'kernels', 'regularized_kernel_pathme_universe.pickle'),
+              show_default=True)
+@click.option('--isolates', is_flag=False, help='Include isolates')
+def regularized_kernel_from_bel_graph(bel_graph_path, output_path, isolates):
+    bel_graph = pybel.from_pickle(bel_graph_path)
 
+    if not isolates:
+        click.echo(f'Number of isolates after getting graph: {nx.number_of_isolates(bel_graph_path)}')
+
+        bel_graph.remove_nodes_from({
+            node
+            for node in nx.isolates(bel_graph)
+        })
+
+    click.echo(bel_graph.summary_str())
+
+    then = time.time()
+    background_mat = regularised_laplacian_kernel(bel_graph)
+    now = time.time()
+    click.echo("It took: ", now - then, " seconds")
+
+    to_pickle(background_mat, os.path.join(output_path))
 
 if __name__ == '__main__':
     main()
