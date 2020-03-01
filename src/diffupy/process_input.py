459a6a1368076cf48fb057823e9fa07c56a6db2a
jmarinllao <josepmarinllao@gmail.com>
2020-03-01 12:43:20 +0100

    Flake8 indentation cleaning

--- src/diffupy/process_input.py
+++ src/diffupy/process_input.py
@@ -5,7 +5,8 @@
 from .matrix import Matrix
 
 
-def generate_categoric_input_vector_from_labels(rows_labeled, col_label, background_mat, missing_value=-1, rows_unlabeled=None):
+def generate_categoric_input_vector_from_labels(rows_labeled, col_label, background_mat, missing_value=-1,
+                                                rows_unlabeled=None):
     if isinstance(col_label, str):
         col_label = [col_label]
 
@@ -15,13 +16,15 @@ def generate_categoric_input_vector_from_labels(rows_labeled, col_label, backgro
     if rows_unlabeled:
         input_mat.row_bind(matrix=Matrix(rows_labels=list(rows_unlabeled),
                                          cols_labels=col_label,
-                                         init_value=0)
-                          )
+                                         init_value=0
+                                         )
+                           )
 
     return input_mat.match_missing_rows(background_mat.rows_labels, missing_value).match_rows(background_mat)
 
 
-def generate_categoric_input_from_labels(rows_labels, cols_labels, background_mat, missing_value=-1, rows_unlabeled=None, ):
+def generate_categoric_input_from_labels(rows_labels, cols_labels, background_mat, missing_value=-1,
+                                         rows_unlabeled=None, ):
     if isinstance(cols_labels, list) and len(cols_labels) > 1:
         input_mat = generate_categoric_input_vector_from_labels(rows_labels[0],
                                                                 cols_labels[0],
