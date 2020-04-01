e4c72938119cfdffc3b48bc37ebee9649321b310
sarah.mubeen <sarah.mubeen@scai.fraunhofer.de>
2020-04-01 12:49:58 +0200

    update input tests

--- tests/test_input.py
+++ tests/test_input.py
@@ -13,85 +13,106 @@ from tests.constants import *
 
 log = logging.getLogger(__name__)
 
-original = {'A': 0.7, 'B': 1.2, 'C': -0.2, 'D': -0.4, 'E': -2.2}
-
 
 class ValidateTest(unittest.TestCase):
     """Test validation of results."""
 
     def test_quantitative_bin_id(self):
-        """Test codify input for quantitative scoring methods- only entity IDs given (binary labels)"""
+        """Test codify input for quantitative scoring methods- only entity IDs given (binary labels)."""
         input = NODE_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=None)
-
+            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=None,
+        )
         self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
 
     def test_quantitative_bin_fc_sign(self):
-        """Test codify input for quantitative scoring methods- logFC given (binary, signed labels)"""
+        """Test codify input for quantitative scoring methods- logFC given (binary, signed labels)."""
         input = NODE_LOGFC_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
 
     def test_quantitative_bin_fc_abs(self):
-        """Test codify input for quantitative scoring methods- logFC given (binary, absolute values)"""
+        """Test codify input for quantitative scoring methods- logFC given (binary, absolute values)."""
         input = NODE_LOGFC_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
 
     def test_quantitative_bin_fcp_sign(self):
-        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (binary, signed labels)"""
+        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (binary, signed labels)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
 
     def test_quantitative_bin_fcp_abs(self):
-        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (binary, absolute values)"""
+        """Test codify input for quant. scoring methods- logFC and adj. p-value given (binary, absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
 
     def test_quantitative_fc_sign(self):
-        """Test codify input for quantitative scoring methods- logFC given (quantitative, signed labels)"""
+        """Test codify input for quantitative scoring methods- logFC given (quantitative, signed labels)."""
         input = NODE_LOGFC_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
 
     def test_quantitative_fc_abs(self):
-        """Test codify input for quantitative scoring methods- logFC given (quant., absolute values)"""
+        """Test codify input for quantitative scoring methods- logFC given (quant., absolute values)."""
         input = NODE_LOGFC_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
 
     def test_quantitative_fcp_sign(self):
-        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (quant., signed labels)"""
+        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (quant., signed labels)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
 
     def test_quantitative_fcp_abs(self):
-        """Test codify input for quantitative scoring methods- logFC and adj. p-value given (quant., absolute values)"""
+        """Test codify input for quant. scoring methods- logFC and adj. p-value given (quant., absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
         input_scores_dict = process_input(
-            input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
 
     def test_non_quantitative_bin_id(self):
-        """Test codify input for non-quantitative scoring methods- only entity IDs given (binary labels)"""
+        """Test codify input for non-quantitative scoring methods- only entity IDs given (binary labels)."""
         input = NODE_TEST_PATH
         input_scores_dict = process_input(
-            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=None,
+        )
+
+        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
 
     def test_non_quantitative_bin_fc_abs(self):
-        """Test codify input for non-quantitative scoring methods- logFC given (binary, absolute values)"""
+        """Test codify input for non-quantitative scoring methods- logFC given (binary, absolute values (sign))."""
         input = NODE_LOGFC_TEST_PATH
         input_scores_dict = process_input(
-            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
 
     def test_non_quantitative_bin_fcp_abs(self):
-        """Test codify input for non-quant. scoring methods- logFC and adj. p-value given (binary, absolute values)"""
+        """Test codify input for non-quant. scoring methods- logFC and adj. p-value given (binary, absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
         input_scores_dict = process_input(
-            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5)
+            input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+        self.assertEqual(input_scores_dict, {'A': -1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
 
     def test_validate_scores_1(self):
         """Test validate scores 1."""
