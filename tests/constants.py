39b2decd370f875e76eff11e6abcb292e52b09c5
sarah.mubeen <sarah.mubeen@scai.fraunhofer.de>
2020-04-02 16:28:13 +0200

    update tests

--- tests/constants.py
+++ tests/constants.py
@@ -20,6 +20,9 @@ NODE_LOGFC_PVAL_TEST_PATH = os.path.join(DATASETS_FOLDER, 'node_logfc_pval.csv')
 INPUT_SCORES = os.path.join(RESOURCES_FOLDER, 'input_scores.csv')
 INPUT_UNLABELED_SCORES = os.path.join(RESOURCES_FOLDER, 'input_unlabeled_scores.csv')
 
+NETWORKS_FOLDER = os.path.join(RESOURCES_FOLDER, 'networks')
+NETWORK_PATH = os.path.join(NETWORKS_FOLDER, 'network_1.csv')
+
 OUTPUT_RAW_SCORES = os.path.join(RESOURCES_FOLDER, 'output_raw_scores.csv')
 OUTPUT_Z_SCORES = os.path.join(RESOURCES_FOLDER, 'output_z_scores.csv')
 OUTPUT_ML_SCORES = os.path.join(RESOURCES_FOLDER, 'output_ml_scores.csv')
--- tests/test_input.py
+++ tests/test_input.py
@@ -7,7 +7,8 @@ import unittest
 
 from diffupy.constants import *
 from diffupy.matrix import Matrix
-from diffupy.process_input import process_input
+from diffupy.process_input import process_input, map_nodes
+from diffupy.utils import process_network
 from diffupy.validate_input import _validate_scores
 from tests.constants import *
 
@@ -20,99 +21,133 @@ class ValidateTest(unittest.TestCase):
     def test_quantitative_bin_id(self):
         """Test codify input for quantitative scoring methods- only entity IDs given (binary labels)."""
         input = NODE_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=None,
         )
-        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
 
     def test_quantitative_bin_fc_sign(self):
         """Test codify input for quantitative scoring methods- logFC given (binary, signed labels)."""
         input = NODE_LOGFC_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
+        self.assertEqual(input_labels_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
 
     def test_quantitative_bin_fc_abs(self):
         """Test codify input for quantitative scoring methods- logFC given (binary, absolute values)."""
         input = NODE_LOGFC_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': 1, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
 
     def test_quantitative_bin_fcp_sign(self):
         """Test codify input for quantitative scoring methods- logFC and adj. p-value given (binary, signed labels)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=True, absolute_value=False, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
+        self.assertEqual(input_labels_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': -1})
 
     def test_quantitative_bin_fcp_abs(self):
         """Test codify input for quant. scoring methods- logFC and adj. p-value given (binary, absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': 0, 'B': 1, 'C': 0, 'D': 0, 'E': 1})
 
     def test_quantitative_fc_sign(self):
         """Test codify input for quantitative scoring methods- logFC given (quantitative, signed labels)."""
         input = NODE_LOGFC_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
+        self.assertEqual(input_labels_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
 
     def test_quantitative_fc_abs(self):
         """Test codify input for quantitative scoring methods- logFC given (quant., absolute values)."""
         input = NODE_LOGFC_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
+        self.assertEqual(input_labels_dict, {'A': 0.7, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
 
     def test_quantitative_fcp_sign(self):
         """Test codify input for quantitative scoring methods- logFC and adj. p-value given (quant., signed labels)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=False, absolute_value=False, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
+        self.assertEqual(input_labels_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': -2.2})
 
     def test_quantitative_fcp_abs(self):
         """Test codify input for quant. scoring methods- logFC and adj. p-value given (quant., absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
+        self.assertEqual(input_labels_dict, {'A': 0, 'B': 1.2, 'C': 0, 'D': 0, 'E': 2.2})
 
     def test_non_quantitative_bin_id(self):
         """Test codify input for non-quantitative scoring methods- only entity IDs given (binary labels)."""
         input = NODE_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=None,
         )
-
-        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1})
 
     def test_non_quantitative_bin_fc_abs(self):
         """Test codify input for non-quantitative scoring methods- logFC given (binary, absolute values (sign))."""
         input = NODE_LOGFC_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': 1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': 1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
 
     def test_non_quantitative_bin_fcp_abs(self):
         """Test codify input for non-quant. scoring methods- logFC and adj. p-value given (binary, absolute values)."""
         input = NODE_LOGFC_PVAL_TEST_PATH
-        input_scores_dict = process_input(
+        input_labels_dict = process_input(
             input, method=ML, binning=True, absolute_value=True, p_value=0.05, threshold=0.5,
         )
-        self.assertEqual(input_scores_dict, {'A': -1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
+        self.assertEqual(input_labels_dict, {'A': -1, 'B': 1, 'C': -1, 'D': -1, 'E': 1})
+
+    def test_network(self):
+        """Test generate graph from csv."""
+        graph = process_network(NETWORK_PATH, CSV)
+        graph_nodes = set(graph.nodes())
+        graph_edges = set(graph.edges())
+
+        self.assertEqual(graph_nodes, {'A', 'B', 'C', 'D', 'E', 'F', 'G'})
+        self.assertEqual(graph_edges, {
+            ('A', 'B'),
+            ('A', 'D'),
+            ('B', 'C'),
+            ('B', 'G'),
+            ('C', 'D'),
+            ('C', 'E'),
+            ('C', 'F'),
+            ('D', 'E'),
+            ('E', 'F'),
+            ('G', 'F'),
+        })
+
+    def test_node_mapping(self):
+        """Test mapping of nodes in input to nodes in network"""
+
+        input = NODE_LOGFC_TEST_PATH
+        input_labels_dict = process_input(
+            input, method=RAW, binning=False, absolute_value=True, p_value=0.05, threshold=0.5,
+        )
+
+        graph = process_network(NETWORK_PATH, CSV)
+        graph_nodes = list(graph.nodes())
+
+        mapped_nodes_list = map_nodes(input_labels_dict, graph_nodes)
+
+        self.assertEqual(mapped_nodes_list, [0.0, 1.0, 0.0, 0.0, None, 1.0, None, None, None])
 
     def test_validate_scores_1(self):
         """Test validate scores 1."""
