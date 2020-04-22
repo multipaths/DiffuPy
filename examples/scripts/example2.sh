d03359080783a74fd1fd042216875e82d4e35656
sarah.mubeen <sarah.mubeen@scai.fraunhofer.de>
2020-04-22 16:13:54 +0200

    update example scripts

--- examples/scripts/example2.sh
+++ examples/scripts/example2.sh
@@ -9,10 +9,12 @@
 # threshold=threshold to apply if logFC in data
 # p_value=statistical significance (default 0.05)
 
-
-
 python3 -m diffupath diffusion diffuse \
   --network ../networks/sample_network_2.csv \
-  --data ../datasets/sample_dataset.csv \
+  --data ../datasets/sample_dataset_with_logfc.csv \
   --method raw \
+  --binarize True \
+  --absolute_value True \
+  --threshold 0.5 \
+  --p_value 0.05 \
 
