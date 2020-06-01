efc3b9279bc6c329c83da2b075cec67514aa2a66
jmarinllao <josepmarinllao@gmail.com>
2020-06-01 10:53:12 +0200

    flake8 correction

--- src/diffupy/process_input.py
+++ src/diffupy/process_input.py
@@ -564,7 +564,6 @@ def mapping_statistics(
         subtotals: Dict[str, int] = None
 ) -> Dict:
     """Calculate mapping descriptive statistics."""
-
     statistics_dict = {}
 
     total_mapping = set()
